ad49785608eacc0cee71fc36412ae02adcc56002
Dobrzhanskij <dobrzhanskij2015@edustyle.info>
2019-12-27 11:24:43 +0300

    Add validation for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :meme
+  validates :username, presence: true, length: {minimum: 1, message: ': имя пользователя должно иметь хотя бы 1 символ'}
+  validates :body, presence: true, length: {minimum: 1, message: ': комментарий не должен быть пустым'}
 end
